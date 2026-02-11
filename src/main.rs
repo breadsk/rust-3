95597a147e42d91b6064609da4fc3153579647e7
Nicolas <nicolas.programador@gmail.com>
2026-02-11 14:31:47 -0300

    variables e interpolacion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     let apples = 50;
+    let oranges = 14 + 6;
+    let _fruits = apples + oranges;
+
+    //println!("This year, my garden has {} apples and {} oranges",apples,oranges);
+    //println!("This year, my garden has {apples} apples and {oranges} oranges");
+    // println!(
+    //     "This year, my garden has {0} apples and {1} oranges",
+    //     apples, oranges
+    // );
 }
